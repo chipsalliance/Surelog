d4d9deb8d36dfed0db4b721389484504809444ea
Alain Dargelas <alainmarcel@yahoo.com>
2019-11-02 17:17:58 -0700

    Travis test

--- SVIncCompil/buildflatbuffer.sh
+++ SVIncCompil/buildflatbuffer.sh
@@ -1,10 +1,12 @@
 #########################################################################
 # Build flatbuffers
 #########################################################################
+echo "Making Flatbuffers"
 mkdir -p ../flatbuffers
 cd ../flatbuffers
 cp -Rf ../third_party/flatbuffers/* .
 cmake -G "Unix Makefiles" -DCMAKE_CXX_FLAGS="-Wimplicit-fallthrough=0"
 make -j 4
 ./flattests
+echo "Done Making Flatbuffers"
 
