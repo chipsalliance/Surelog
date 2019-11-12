0f9e088281bec14d261265ad34008dd4b9380bf4
Alain Dargelas <alainmarcel@yahoo.com>
2019-11-11 21:21:09 -0800

    g++ version debug

--- SVIncCompil/buildflatbuffer.sh
+++ SVIncCompil/buildflatbuffer.sh
@@ -11,9 +11,11 @@ echo $?
 mkdir -p ../flatbuffers
 cd ../flatbuffers
 cp -Rf ../third_party/flatbuffers/* .
-cmake -G "Unix Makefiles"  -DCMAKE_CXX_FLAGS="-w -Wimplicit-fallthrough=0"  &>  flatbuffers_configure.log
-make -j 4 &>  flatbuffers_compile.log
-./flattests  &>  flatbuffers_test.log
+export LD_LIBRARY_PATH=/usr/local/lib64/:/usr/lib64/:$LD_LIBRARY_PATH
+export PATH=/usr/local/bin/:$PATH
+cmake -G "Unix Makefiles"  -DCMAKE_CXX_FLAGS="-w -Wimplicit-fallthrough=0" # &>  flatbuffers_configure.log
+make -j 4 #&>  flatbuffers_compile.log
+./flattests  #&>  flatbuffers_test.log
 
 echo "Done Building Flatbuffers"
 
