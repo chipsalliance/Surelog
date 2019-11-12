3d0b312bbe3d5a7d8e6f3fe11c6d3f28b980190e
Alain Dargelas <alainmarcel@yahoo.com>
2019-11-11 22:13:56 -0800

    g++ version debug

--- SVIncCompil/buildflatbuffer.sh
+++ SVIncCompil/buildflatbuffer.sh
@@ -10,13 +10,12 @@ mkdir -p ../flatbuffers
 cd ../flatbuffers
 cp -Rf ../third_party/flatbuffers/* .
 export LD_LIBRARY_PATH=/usr/local/lib64/:/usr/lib64/:$LD_LIBRARY_PATH
-export PATH=/usr/local/bin/:$PATH
-which g++
+
+export CXX=`which g++` ; export CC=`which gcc`
+[ -f /usr/bin/g++-7 ] && export CXX=`which g++-7` ; export CC=`which gcc-7`
+
+$CXX --version
 echo $?
-g++ --version
-echo $?
-export CXX=`which g++`
-export CC=`which gcc`
 
 cmake -G "Unix Makefiles"  -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CXX_FLAGS="-w -Wimplicit-fallthrough=0"  #&>  flatbuffers_configure.log
 make -j 4 #&>  flatbuffers_compile.log
