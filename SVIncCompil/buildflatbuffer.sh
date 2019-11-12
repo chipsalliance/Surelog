0de33b302f5690291b6c670f2ab3570f0115969d
Alain Dargelas <alainmarcel@yahoo.com>
2019-11-11 22:29:55 -0800

    g++ version debug

--- SVIncCompil/buildflatbuffer.sh
+++ SVIncCompil/buildflatbuffer.sh
@@ -9,8 +9,8 @@ echo "Building Flatbuffers"
 mkdir -p ../flatbuffers
 cd ../flatbuffers
 cp -Rf ../third_party/flatbuffers/* .
-export LD_LIBRARY_PATH=/usr/local/lib64/:/usr/lib64/:$LD_LIBRARY_PATH
 
+export LD_LIBRARY_PATH=/usr/local/lib64/:/usr/lib64/:$LD_LIBRARY_PATH
 export CXX=`which g++` ; export CC=`which gcc`
 [ -f /usr/bin/g++-7 ] && export CXX=`which g++-7` ; export CC=`which gcc-7`
 
--- SVIncCompil/buildrelease.sh
+++ SVIncCompil/buildrelease.sh
@@ -4,14 +4,14 @@ set -e
 
 # Complete Surelog build script (Only builds the release executable)
 echo "Building Surelog"
+
 export LD_LIBRARY_PATH=/usr/local/lib64/:/usr/lib64/:$LD_LIBRARY_PATH
-export PATH=/usr/local/bin/:$PATH
-which g++
+export CXX=`which g++` ; export CC=`which gcc` ; export GPP=`which g++`
+[ -f /usr/bin/g++-7 ] && export CXX=`which g++-7` ; export CC=`which gcc-7` ; export GPP=`which g++-7`
+[ -f /usr/local/bin/g++-7 ] && export CXX=`which g++-7` ; export CC=`which gcc-7` ; export GPP=`which g++-7`
+
+$CXX --version
 echo $?
-g++ --version
-echo $?
-export CXX=`which g++`
-export CC=`which gcc`
 
 echo "Generating Antlr parser"
 cd ../G4
@@ -35,7 +35,7 @@ API/generate_python_listener_api.tcl
 API/embed_python_api.tcl
 
 echo "Make"
-make CONF=Release -j 4;
+make CONF=Release -j 4 CXX='${GPP} -Wall -O3 -fno-asm -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -Wno-attributes';
 echo "Done Building Surelog"
 
 echo "Run Tests"
