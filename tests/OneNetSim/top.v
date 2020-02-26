e18cdda545bd67262c9a8c360f8742af232658fb
Alain <alainmarcel@yahoo.com>
2020-02-25 22:27:27 -0800

    OneNetSim support of module/program instantiations in UHDM
    Signed-off-by: Alain <alainmarcel@yahoo.com>

--- tests/OneNetSim/top.v
+++ tests/OneNetSim/top.v
@@ -1,6 +1,5 @@
-program TESTBENCH(input observe, output drive);
-  reg drive;
-  wire observe;
+
+program TESTBENCH(input wire observe, output reg drive);
   initial begin
     $monitor("@%0dns observe = %0d",$time,observe);
     drive = 0;
@@ -9,11 +8,15 @@ program TESTBENCH(input observe, output drive);
 endprogram
 
 module DUT (input wire i, output reg o);
+  SUB sub1(i,o);
+endmodule
+
+module SUB (input wire i, output reg o);
   assign o = i;
 endmodule
 
 module TOP();
   wire i,o;
   DUT dut(i,o);
-  TESTBENCH(o,i);
+  TESTBENCH tb(o,i);
 endmodule
