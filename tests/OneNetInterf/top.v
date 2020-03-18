ecc360a0bf2afe43091babc2f4a7cade0d5f0afc
Alain <alainmarcel@yahoo.com>
2020-03-17 21:28:33 -0700

    elab interface in uhdm

--- tests/OneNetInterf/top.v
+++ tests/OneNetInterf/top.v
@@ -1,4 +1,4 @@
-
+/*
 program TESTBENCH(input wire observe, output reg drive);
   initial begin
     $monitor("@%0dns observe = %0d",$time,observe);
@@ -6,11 +6,11 @@ program TESTBENCH(input wire observe, output reg drive);
     #100 drive = 1;
   end
 endprogram
-
+*/
 
 interface ConnectTB (input wire con_i, output reg con_o) ;
 endinterface
-
+/*
 module DUT (ConnectTB conn);
   SUB sub1(.inp(conn.con_i),.out(conn.con_o));
 endmodule
@@ -18,10 +18,10 @@ endmodule
 module SUB (input wire inp, output reg out);
   assign out = inp;
 endmodule
-
+*/
 module TOP();
   wire i,o;
   ConnectTB conntb(.con_i(i),.con_o(o));
-  DUT dut(conntb);
-  TESTBENCH tb(.observe(conntb.con_o),.drive(conntb.con_i));
+  //DUT dut(conntb);
+  //TESTBENCH tb(.observe(conntb.con_o),.drive(conntb.con_i));
 endmodule
