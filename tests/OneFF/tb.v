4f3ecd1192db30e31bdf88b55d4a7aa446ed7340
Alain Dargelas <alainmarcel@yahoo.com>
2020-04-06 18:43:13 -0700

    Fix a syntax error for Verilator

--- tests/OneFF/tb.v
+++ tests/OneFF/tb.v
@@ -8,7 +8,7 @@ module tb;
   initial begin
     $dumpfile("test.vcd");
     $dumpvars;
-    $monitor("@%0dns clk = %0d, %0d",$time,clk, o);
+    $monitor("@%0dns clk = %0d, %0d",$time,clk, o);   
     #100 $finish();
   end
   
@@ -18,11 +18,11 @@ module tb;
     clk = 0; 
     d = 0;  
     #2 rstn = 1;
-    assert(o == 0) $display("OK!") else $fatal(1, "FAILED ASSERTION");  
+    assert(o == 0) $display("OK!"); else $fatal(1, "FAILED ASSERTION");  
     #10 d = 1;
-    #5 assert(o == 1) $display("OK!") else $fatal(1, "FAILED ASSERTION");  
+    #5 assert(o == 1) $display("OK!"); else $fatal(1, "FAILED ASSERTION");  
     #20 d = 0; 
-    #10 assert(o == 0) $display("OK!") else $fatal(1, "FAILED ASSERTION");   
+    #10 assert(o == 0) $display("OK!"); else $fatal(1, "FAILED ASSERTION");   
   end 
     
   always 
