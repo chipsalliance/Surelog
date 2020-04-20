261fcfe57e4014389fb4ea252be0684182563770
Tim 'mithro' Ansell <me@mith.ro>
2020-04-20 15:18:49 -0700

    Adding gcc-7 as the compiler.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .github/kokoro/steps/hostsetup.sh
+++ .github/kokoro/steps/hostsetup.sh
@@ -38,6 +38,23 @@ sudo apt-get install -y \
         tclsh \
         uuid-dev \
 
+
+sudo apt-get install -y \
+	gcc-7 \
+	libgcc-7-dev \
+
+
+sudo update-alternatives --remove-all gcc || true
+sudo update-alternatives --remove-all g++ || true
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 10
+
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+sudo update-alternatives --set cc /usr/bin/gcc
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+sudo update-alternatives --set c++ /usr/bin/g++
+
+
 if [ -z "${BUILD_TOOL}" ]; then
     export BUILD_TOOL=make
 fi
