25ffdd181a37c82fce947bcc657d3d1442a55b46
Henner Zeller <h.zeller@acm.org>
2020-05-14 08:54:36 -0700

    Use uhdm-dump from UHDM compile.
    This is an attempt to work-around #376
    
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- tests/regression.tcl
+++ tests/regression.tcl
@@ -147,12 +147,13 @@ if [regexp {commit=([A-Za-z0-9_ \.]+)} $argv tmp COMMIT_TEXT] {
 }
 
 set EXE_PATH "[pwd]/dist/$BUILD"
+set THIRD_PARTY_PATH "[pwd]/third_party"
 
 if [regexp {path=([A-Za-z0-9_/\.-]+)} $argv tmp EXE_PATH] {
 }
 
 set SURELOG_VERSION "$EXE_PATH/surelog"
-set UHDM_DUMP_COMMAND "$EXE_PATH/uhdm-dump"
+set UHDM_DUMP_COMMAND "$THIRD_PARTY_PATH/UHDM/uhdm-dump"
 
 if ![file exist $SURELOG_VERSION] {
     puts "ERROR: Cannot find executable $SURELOG_VERSION!"
