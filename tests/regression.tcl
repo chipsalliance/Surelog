ea0c7d45fa2c47de0989b49edaa7a8554e420ae4
hs <hs@davenche.com>
2020-05-14 18:45:22 -0700

    Issue #376 Fix to incorporate changes to UHDM cmake file.
    Highlights -
    * Generate output files in location where the dev tools expect
      instead of moving them to custom location. Rationale being that
      dev tools work better when the files aren't moved around.
      Visual Studio fails to find the generated test binaries if
      they aren't in the expected location. Plus, on windows, CMake
      works in multi-configuration mode and so having a single "dist"
      folder doesn't work well.
    * Upgrade minimum CMake version required to 3.15
      This is required for the policy CMP0091 which was introduced in
      this version.
    * Also, upgraded minimum C++ version to 17 to support filesystem
      module. This change will help cross-platform support.
    
    IMPORTANT: This change is blocked by -
    https://github.com/alainmarcel/UHDM/pull/236

--- tests/regression.tcl
+++ tests/regression.tcl
@@ -146,7 +146,7 @@ set COMMIT_TEXT ""
 if [regexp {commit=([A-Za-z0-9_ \.]+)} $argv tmp COMMIT_TEXT] {
 }
 
-set EXE_PATH "[pwd]/dist/$BUILD"
+set EXE_PATH "[pwd]/bin"
 
 if [regexp {path=([A-Za-z0-9_/\.-]+)} $argv tmp EXE_PATH] {
 }
