81f0464757afb0e3c9c3b52b5a294f5e2406fd49
Alain Dargelas <alain.dargelas@gmail.com>
2020-05-31 18:22:59 -0700

    Fixed bad usage of FileContent::Line

--- src/DesignCompile/DesignElaboration.cpp
+++ src/DesignCompile/DesignElaboration.cpp
@@ -1278,7 +1278,7 @@ void DesignElaboration::collectParams_(std::vector<std::string>& params,
         NodeId expr = parentFile->Sibling(child);
         Value* value =
             m_exprBuilder.evalExpr(parentFile, expr, instance->getParent());
-        instance->setValue(name, value, m_exprBuilder, fC->Line(expr));
+        instance->setValue(name, value, m_exprBuilder, parentFile->Line(expr));
       } else {
         // Index param
         NodeId expr = child;
@@ -1294,7 +1294,7 @@ void DesignElaboration::collectParams_(std::vector<std::string>& params,
           Error err(ErrorDefinition::ELAB_OUT_OF_RANGE_PARAM_INDEX, loc);
           errors->addError(err);
         }
-        instance->setValue(name, value, m_exprBuilder, fC->Line(expr));
+        instance->setValue(name, value, m_exprBuilder, parentFile->Line(expr));
         index++;
       }
     }
