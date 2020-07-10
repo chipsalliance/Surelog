fd698477a59d6aa5fd42683b0e45ec5dcf9b52a3
Alain Dargelas <alain.dargelas@gmail.com>
2020-07-10 10:28:48 -0700

    typedefs scope

---
+++ tests/TypeDefScope/dut.sv
@@ -0,0 +1,19 @@
+module dut (a, b);
+
+   function void UVMC_set_config_object (string type_name,
+      string contxt,
+      string inst_name,
+      string field_name,
+      bits_t value);
+typedef uvmc_default_converter #(uvm_object) converter1;
+typedef logic converter2;
+
+uvm_object obj;
+uvm_component comp;
+
+
+   endfunction
+
+
+ endmodule
+ 
