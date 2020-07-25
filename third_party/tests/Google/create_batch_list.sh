9244a8295a9a38440f6895e0fa8ddb4ed9699eef
Alain Dargelas <alain.dargelas@gmail.com>
2020-07-24 21:37:36 -0700

    Verilator test ocnverted to batch test

--- third_party/tests/Google/create_batch_list.sh
+++ third_party/tests/Google/create_batch_list.sh
@@ -1,1 +1,1 @@
- ../../../tests/create_batch_script.tcl  ext=.v,.sv batch=batch.txt options="-parse -nocache -nobuiltin -noinfo -noelab -timescale=1ns/1ns -nostdout"
+ ../../../tests/create_batch_script.tcl  ext=.v,.sv batch=batch.txt options="-parse -nocache -nobuiltin -nonote -noinfo -timescale=1ns/1ns"
---
+++ third_party/tests/Verilator/create_batch_list.sh
@@ -0,0 +1,1 @@
+ ../../../tests/create_batch_script.tcl  ext=.v,.sv batch=batch.txt options="+incdir+. -parse -noelab -nocache -nobuiltin -nonote -noinfo -sverilog -timescale=1ns/1ns"
