367b75a684c460236a38e427be4f77fd0721233d
Alain Dargelas <alain.dargelas@gmail.com>
2020-11-07 00:14:34 -0800

    test for parameterized class passed as class param

---
+++ tests/ClassParamAsParam/dut.sv
@@ -0,0 +1,19 @@
+virtual class object#(type T=int);
+endclass
+
+class resource #(type T=int);
+endclass
+
+class resource_db #(type T=int);
+
+ typedef resource #(T) rsrc_t;
+ rsrc_t rsrc;
+ 
+endclass
+
+class config_db#(type T=int) extends resource_db#(T);
+endclass
+
+module top ();
+  config_db#(object#(long)) misc2 = new;
+endmodule
