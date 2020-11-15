4b4f9b36230262cf1e34814c46b9db126678b4e5
Alain Dargelas <alain.dargelas@gmail.com>
2020-11-14 22:39:13 -1000

    Better net/var type

---
+++ tests/LogicTypedef/dut.sv
@@ -0,0 +1,13 @@
+module test;
+    typedef logic [1:0] log_two_bits;
+    log_two_bits logn;
+
+    typedef reg [1:0] reg_two_bits;
+    reg_two_bits regn;
+
+    typedef bit [1:0] bit_two_bits;
+    bit_two_bits bitn;
+
+endmodule
+
+
