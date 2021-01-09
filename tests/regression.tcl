7d6df7001189a8dcea7c8e065d43e242f143b746
Alain Dargelas <alain.dargelas@gmail.com>
2021-01-09 13:14:56 -1000

    remove tests from Windows build

--- tests/regression.tcl
+++ tests/regression.tcl
@@ -186,6 +186,8 @@ dict set WINDOWS_BLACK_LIST Verilator 1
 dict set WINDOWS_BLACK_LIST BuildUVMPkg 1
 dict set WINDOWS_BLACK_LIST Compl1001 1
 dict set WINDOWS_BLACK_LIST YosysOpenSparc 1
+dict set WINDOWS_BLACK_LIST Earlgrey_Verilator_0_1 1
+dict set WINDOWS_BLACK_LIST Earlgrey_Verilator_01_05_21 1
 
 set UNIX_BLACK_LIST [dict create]
 # 2 message diff:
