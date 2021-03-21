a3064973ea1456dba830745c3f6816ed8c6e982c
Alain Dargelas <alain.dargelas@gmail.com>
2021-03-21 11:05:35 -1000

    Re-enable code coverage on BlackParrot

--- third_party/tests/black-parrot/bp_top/syn/runsurelog.sh
+++ third_party/tests/black-parrot/bp_top/syn/runsurelog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 { # try
-
-   make build_cov.sc sim.sc SUITE=riscv_tests PROG=rsort VERILATOR="$1 -sverilog -parse -nopython -verbose -timescale=1ps/1ps -verbose"  && echo "OK"
+   make build_cov.sc sim.sc SUITE=riscv_tests PROG=rsort
+   make build_cov.sc sim.sc SUITE=riscv_tests PROG=rsort VERILATOR="$1 -sverilog -parse -nopython -verbose -timescale=1ps/1ps -elabuhdm -d coveruhdm -verbose"  && echo "OK"
     #save your output
 
 } || { # catch
