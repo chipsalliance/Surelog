b236244317af3d747e724c63715408b4028c89f6
Alain Dargelas <alain.dargelas@gmail.com>
2021-04-11 21:51:27 -1000

    UHDM stages messages

--- third_party/tests/black-parrot/bp_top/syn/runsurelog.sh
+++ third_party/tests/black-parrot/bp_top/syn/runsurelog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 { # try
+   rm -rf results/ simsc/slpp_all
    make build_cov.sc sim.sc SUITE=riscv_tests PROG=rsort
+   rm -rf results/ simsc/slpp_all
    make build_cov.sc sim.sc SUITE=riscv_tests PROG=rsort VERILATOR="$1 -sverilog -parse -nopython -verbose -timescale=1ps/1ps -elabuhdm -d coveruhdm -verbose"  && echo "OK"
     #save your output
 
