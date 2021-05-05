8e10e096cb1b44d0910bc86157776053c382cd0b
Alain Dargelas <alain.dargelas@gmail.com>
2021-05-04 21:08:31 -1000

    Try to reenable some tests on Windows using lowmem option

--- tests/regression.tcl
+++ tests/regression.tcl
@@ -179,8 +179,8 @@ set SURELOG_COMMAND "$TIME $DEBUG_TOOL $SURELOG_VERSION"
 
 set WINDOWS_BLACK_LIST [dict create]
 dict set WINDOWS_BLACK_LIST Ariane 1
-dict set WINDOWS_BLACK_LIST BlackParrot 1
-dict set WINDOWS_BLACK_LIST BlackPBe 1
+#dict set WINDOWS_BLACK_LIST BlackParrot 1
+#dict set WINDOWS_BLACK_LIST BlackPBe 1
 dict set WINDOWS_BLACK_LIST CoresSweRV 1
 dict set WINDOWS_BLACK_LIST SimpleIncludeAndMacros 1
 dict set WINDOWS_BLACK_LIST TestFileSplit 1
@@ -193,16 +193,16 @@ dict set WINDOWS_BLACK_LIST Compl1001 1
 dict set WINDOWS_BLACK_LIST YosysOpenSparc 1
 dict set WINDOWS_BLACK_LIST Earlgrey_Verilator_0_1 1
 dict set WINDOWS_BLACK_LIST Earlgrey_Verilator_01_05_21 1
-dict set WINDOWS_BLACK_LIST Earlgrey_nexysvideo 1
+#dict set WINDOWS_BLACK_LIST Earlgrey_nexysvideo 1
 
 set UNIX_BLACK_LIST [dict create]
 # 2 message diff:
 dict set UNIX_BLACK_LIST UnitElabExternNested 1
 
 # RAM size in CI machines
-dict set UNIX_BLACK_LIST Earlgrey_nexysvideo 1
-dict set UNIX_BLACK_LIST BlackParrot 1
-dict set UNIX_BLACK_LIST BlackPBe 1
+#dict set UNIX_BLACK_LIST Earlgrey_nexysvideo 1
+#dict set UNIX_BLACK_LIST BlackParrot 1
+#dict set UNIX_BLACK_LIST BlackPBe 1
 
 if { $tcl_platform(platform) == "windows" } {
     set BLACK_LIST $WINDOWS_BLACK_LIST
