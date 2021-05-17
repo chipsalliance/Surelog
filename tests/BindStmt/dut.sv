b7def5ffff088691623b22475eac8104c3483669
Alain Dargelas <alain.dargelas@gmail.com>
2021-05-16 15:06:48 -1000

    binding our of order

--- tests/BindStmt/dut.sv
+++ tests/BindStmt/dut.sv
@@ -2,15 +2,33 @@ module bp_lce #(parameter sets_p = "inv")(input logic a, output logic b);
 assign c =d;
 endmodule
 
+module sub();
+endmodule
+
 module bp_me_nonsynth_lce_tracer #(parameter sets_p = "inv")(input logic c, output logic d);
   assign a =b;
+  sub sub1();
 endmodule
 
 module bp_me_nonsynth_lce_tracer2 #(parameter sets_p = "inv")(input logic a, output logic b, input logic clk);
   assign a =b;
+  sub sub1();
+endmodule
+
+
+module inter ();
+
+ bp_lce #(.sets_p(1)) u1 ();
+
 endmodule
 
 module testbench (input logic clk);
+
+  bp_lce #(.sets_p(1)) u1 ();
+  inter tt ();
+
+  parameter p = 0;
+  if (p == 0) begin : do_bind
 
   bind bp_lce
             bp_me_nonsynth_lce_tracer
@@ -28,7 +46,8 @@ module testbench (input logic clk);
               #(.sets_p(sets_p))
               lce_tracer3();  
 
+  end
 
-  bp_lce #(.sets_p(1)) u1 ();
-
+ 
 endmodule
+
