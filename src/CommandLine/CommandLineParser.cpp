3e82162eb4ba384b78705fee53bf1411e2b1577f
Alain Dargelas <alain.dargelas@gmail.com>
2021-05-25 19:03:27 -1000

    import stmt in interfaces and programs

--- src/CommandLine/CommandLineParser.cpp
+++ src/CommandLine/CommandLineParser.cpp
@@ -52,7 +52,7 @@
 using namespace SURELOG;
 
 static std::string defaultLogFileName = "surelog.log";
-std::string CommandLineParser::m_versionNumber = "1.02";
+std::string CommandLineParser::m_versionNumber = "1.03";
 
 static const std::vector<std::string> copyright = {
     "Copyright (c) 2017-2021 Alain Dargelas,",
