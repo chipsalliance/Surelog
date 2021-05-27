c2acb8ded97cd198d7df6ed04602f56d5a15716f
Alain Dargelas <alain.dargelas@gmail.com>
2021-05-26 18:45:20 -1000

    utils

---
+++ src/utils/README.md
@@ -0,0 +1,8 @@
+# Utilities used to compare Verilator and Surelog hierarchy
+
+* 1) verilator.tcl takes a xxx_expand.tree from Verilator obtained with the --tree-dump and extracts the hierarchy paths
+
+* 2) Use Surelog/build/third_party/bin/uhdm-hier surelog.uhdm to dump Surelog hierarchy
+* sort.tcl lexical sort Surelog output
+
+* 3) diff.tcl <from> <to> displays missing entries in <to> taken from <from>
---
+++ src/utils/verilator.tcl
@@ -0,0 +1,27 @@
+#!/usr/bin/tclsh
+
+proc parse { file } {
+    puts "Parsing $file"
+    set fid [open $file]
+    set content [read $fid]
+    set lines [split $content "\n"]
+    set fod [open "hierarchy.txt" "w"]
+    foreach line $lines {
+	if [regexp {(chip_earlgrey_verilator__DOT__[a-zA-Z0-9_]*)} $line tmp path] {
+	    regsub -all {__DOT__} $path "." path 
+	    regsub -all {__BRA__} $path "\[" path 
+	    regsub -all {__KET__} $path "\]" path 
+	    regsub {\.[a-zA-Z0-9_]*$} $path "" path
+	    set PATHS($path) 1
+	}
+    }
+    close $fid
+
+    foreach path [lsort -dictionary [array names PATHS]] {
+	puts $fod $path
+    }
+    close $fod
+    puts "Wrote hierarchy in hierarchy.txt" 
+}
+
+parse $argv
