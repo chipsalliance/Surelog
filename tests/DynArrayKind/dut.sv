2e82af02966b954f233e3a9edba7565617584709
Alain Dargelas <alain.dargelas@gmail.com>
2021-10-06 16:58:06 -0700

    array dimensions

--- tests/DynArrayKind/dut.sv
+++ tests/DynArrayKind/dut.sv
@@ -1,5 +1,8 @@
 module top;
-   int dynamic [];
+   int dynamic1 [];
+   int dynamic2 [][1:0];
+   int dynamic3 [1:0][];
+   int dynamic4 [1:0][][2:0];
    int assoc [int];
    int assoc_string [string];
    int queue [$];
