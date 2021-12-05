4d308269c31d40a8189122bb438fad86e56fd580
Alain Dargelas <alain.dargelas@gmail.com>
2021-12-04 21:52:32 -0800

    randsequence

--- src/CommandLine/CommandLineParser.cpp
+++ src/CommandLine/CommandLineParser.cpp
@@ -66,7 +66,7 @@ static std::string_view defaultLogFileName = "surelog.log";
 //         Or when the cache schema changes
 //        This will render the cache invalid
 const std::string& CommandLineParser::getVersionNumber() {
-  static const std::string m_versionNumber = "1.14";
+  static const std::string m_versionNumber = "1.15";
   return m_versionNumber;
 }
 
