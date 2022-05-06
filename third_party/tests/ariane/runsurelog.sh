08234f90cae158884970bb3346680e4372331a10
Henner Zeller <hzeller@google.com>
2022-05-06 10:41:27 -0700

    Resolve shell-utilities such as time and bash with /usr/bin/env
    A strict posix system does not guarantee that the utilities
    are available in /bin/bash or in the path, but should be
    resolved by /usr/bin/env
    
    While at it, make tcl regression more robust by choosing
    the _last_ element in the SURELOG_COMMAND with lindex
    instead of a hard-coded index (as this will change with
    a double-element 'time' or even with a debug tool).
    
    Signed-off-by: Henner Zeller <hzeller@google.com>

--- third_party/tests/ariane/runsurelog.sh
+++ third_party/tests/ariane/runsurelog.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 make RISCV=blah verilator="$1 -DVERILATOR=1 -sverilog -parse -d coveruhdm -verbose -timescale=1ps/1ps" verilate CFLAGS="" LDFLAGS=""
--- third_party/tests/black-parrot/bp_be/syn/runsurelog.sh
+++ third_party/tests/black-parrot/bp_be/syn/runsurelog.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 { # try
    make build_cov.sc sim.sc SUITE=riscv_tests PROG=rsort VERILATOR="$1 -sverilog -parse -nopython -verbose -timescale=1ps/1ps -elabuhdm -d coveruhdm -verbose -lowmem"  && echo "OK"
     #save your output
