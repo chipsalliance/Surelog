eeb74fad94f3187b98ea138070da5f8568cb1cbd
Alain Dargelas <alain.dargelas@gmail.com>
2022-06-05 10:24:31 -0700

    select hier path

--- src/DesignCompile/CompileExpression.cpp
+++ src/DesignCompile/CompileExpression.cpp
@@ -4844,8 +4844,8 @@ UHDM::any *CompileHelper::compileComplexFuncCall(
               if (index) {
                 bit_select *select = s.MakeBit_select();
                 elems->push_back(select);
-                //if (!tmpName.empty()) select->VpiParent(path);
-                ref_obj* ref = s.MakeRef_obj();
+                // if (!tmpName.empty()) select->VpiParent(path);
+                ref_obj *ref = s.MakeRef_obj();
                 ref->VpiParent(path);
                 ref->VpiName(tmpName);
                 select->VpiParent(ref);
