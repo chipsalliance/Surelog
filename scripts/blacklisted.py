660ee101b06b59ef95142f74328ed0d4bf9f5600
HS <hs@apotell.com>
2022-11-03 02:51:38 -0700

    Re-enable some of the tests that were disabled without any comment

--- scripts/blacklisted.py
+++ scripts/blacklisted.py
@@ -4,56 +4,29 @@ import platform
 # If you are adding a new entry, please include a short comment
 # explaining why the specific test is blacklisted.
 
-
-_windows_black_list = set([name.lower() for name in [
-    r'ariane',
-    r'blackparrot',
-    r'blackucode',
-    r'blackunicore',
-    r'builduvmpkg',
-    r'compl1001',
-    r'coresswerv',
-    r'earlgrey_nexysvideo',
-    r'earlgrey_verilator_0_1',
-    r'earlgrey_verilator_01_05_21', # lowmem is unsupported
-    r'lpddr',
-    r'simpleincludeandmacros',
-    r'simpleparsertestcache', # race condition
-    r'testfilesplit',
-    r'testsepcompnohash',     # Uses shell script to run
-    r'unitelabexternnested',
-    r'unitpython',
-    r'unitsimpleincludeandmacros',
-    r'verilator',
-    r'yosysopensparc',
-    r'testnohash'
-]])
-
 _unix_black_list = set([name.lower() for name in [
-    r'blackparrot',
-    r'blackucode',
-    r'blackunicore',
-    r'lpddr',
-    r'simpleparsertestcache', # race condition
-    r'earlgrey_nexysvideo',   # ram size in ci machines
-    r'unitelabexternnested',  # 2 message diff:
+  'blackparrot',
+  'blackucode',
+  'blackunicore',
+  'earlgrey_nexysvideo',   # ram size in ci machines
+  'lpddr',
+  'simpleparsertestcache', # race condition
 ]])
 
-_msys2_black_list = _unix_black_list.union([
-    r'earlgrey_verilator_01_05_21', # lowmem is unsupported
-])
-
-# Temporarily disabled on CI as this test seems to be stalling
-_unix_ci_black_list = _unix_black_list.union(set([name.lower() for name in [
-#     'rsd',
-#     'compl1001',
-#     'google',
-#     'unisim'
+_windows_black_list = _unix_black_list.union(set([name.lower() for name in [
+  'ariane',                       # Uses shell script with make command
+  'earlgrey_verilator_01_05_21',  # lowmem is unsupported
+  'unitpython',                   # Python is unsupported
 ]]))
 
+_msys2_black_list = _unix_black_list.union(set([name.lower() for name in [
+  'earlgrey_verilator_01_05_21', # lowmem is unsupported
+]]))
+
+
 def is_blacklisted(name):
-    if platform.system() == 'Windows':
-        blacklist = _msys2_black_list if 'MSYSTEM' in os.environ else _windows_black_list
-    else:
-        blacklist = _unix_ci_black_list if 'GITHUB_JOB' in os.environ else _unix_black_list
-    return name.lower() in blacklist
+  if platform.system() == 'Windows':
+    blacklist = _msys2_black_list if 'MSYSTEM' in os.environ else _windows_black_list
+  else:
+    blacklist = _unix_black_list
+  return name.lower() in blacklist
