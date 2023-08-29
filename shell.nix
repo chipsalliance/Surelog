3f6fd5f931cc6cd13049a6c3cd6773254aa57d8d
Henner Zeller <hzeller@google.com>
2023-08-29 02:10:23 -0700

    Include config.h where needed.
    Also remove SURELOG_SYMBOLID_DEBUG_ENABLED define which is not
    used anymore.

--- shell.nix
+++ shell.nix
@@ -39,9 +39,6 @@ pkgs.mkShell {
     ];
   shellHook = ''
     export CMAKE_CXX_COMPILER_LAUNCHER=ccache
-    export ADDITIONAL_CMAKE_OPTIONS="-DSURELOG_USE_HOST_GTEST=On -DSURELOG_USE_HOST_ANTLR=On -DANTLR_JAR_LOCATION=${pkgs.antlr4.jarLocation}"
-
-    # For the UHDM dependency: tell it to use local capnp
-    export ADDITIONAL_CMAKE_OPTIONS="$ADDITIONAL_CMAKE_OPTIONS -DUHDM_USE_HOST_CAPNP=On"
+    export ADDITIONAL_CMAKE_OPTIONS="-DSURELOG_USE_HOST_GTEST=On -DSURELOG_USE_HOST_CAPNP=On -DSURELOG_USE_HOST_ANTLR=On -DANTLR_JAR_LOCATION=${pkgs.antlr4.jarLocation}"
   '';
 }
